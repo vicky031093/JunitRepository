14d33ccb6073901d19f0bfb09e928341c0db7546
vicky031093 <vigneshpasupathy93@gmail.com>
2021-10-11 16:52:07 +0530

    Project Push

---
+++ src/test/java/org/testng/xml/ClasB.java
@@ -0,0 +1,17 @@
+package org.testng.xml;
+
+import org.testng.annotations.Test;
+
+public class ClasB {
+	@Test
+	private void tc1() {
+		System.out.println("TC1");
+
+	}
+	@Test
+	private void tc2() {
+		System.out.println("TC2");
+
+	}
+
+}
---
+++ src/test/java/org/testng/xml/ClassA.java
@@ -0,0 +1,17 @@
+package org.testng.xml;
+
+import org.testng.annotations.Test;
+
+public class ClassA {
+	@Test
+	private void tc1() {
+		System.out.println("TC1");
+
+	}
+	@Test
+	private void tc2() {
+		System.out.println("TC2");
+
+	}
+
+}
